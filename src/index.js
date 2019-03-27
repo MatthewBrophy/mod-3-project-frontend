a2ada9243f4b56c4addd160add9fc2e7aee2c3f1
elimerrell <elijah.merrell@gmail.com>
2019-03-27 14:21:44 -0700

    added home logo

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@
 let body = document.getElementById('body');
 let welcomeDiv = document.getElementById('main-button');
 let startButton = document.getElementById('main-button');
+let mainImage = document.getElementById('main-image');
 startButton.addEventListener('click', function() {
+	mainImage.innerHTML = '';
 	body.style.overflow = 'auto';
 	welcomeDiv.remove();
 	renderPage();
